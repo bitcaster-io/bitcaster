28a7c05b66729ec8cad6838f31bae72c5d5d28a5
sax <s.apostolico@gmail.com>
2018-02-27 22:28:03 +0100

    updates

--- src/mercury/web/urls.py
+++ src/mercury/web/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import HomeView, LoginView, LogoutView
+from .views import (ApplicationDetail, HomeView, LoginView,
+                    LogoutView, SubscriptionList,)
 
 urlpatterns = [
     path(r'login/', LoginView.as_view(), name='login'),
     path(r'logout/', LogoutView.as_view(), name='logout'),
+    path(r'app/<int:pk>/', ApplicationDetail.as_view(), name='app-index'),
+    path(r'subscriptions/', SubscriptionList.as_view(), name='user-subscriptions'),
     path(r'', HomeView.as_view())
 ]
--- src/mercury/web/views.py
+++ src/mercury/web/views.py
@@ -1,19 +1,39 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import (LoginView as _LoginView,
                                        LogoutView as _LogoutView,)
-from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, ListView, TemplateView
+
+from mercury.models import Application, Subscription
 
 
 class LogoutView(_LogoutView):
     next_page = 'login'
 
 
+class ApplicationListMixin:
+    def get_context_data(self, **kwargs):
+        ret = super().get_context_data(**kwargs)
+        ret['applications'] = Application.objects.all()
+        return ret
+
+
 class LoginView(_LoginView):
     template_name = 'bitcaster/login.html'
 
 
-class HomeView(TemplateView):
+class HomeView(ApplicationListMixin, TemplateView):
     template_name = 'bitcaster/index.html'
 
-    def get_context_data(self, **kwargs):
-        ret = super().get_context_data(**kwargs)
-        return ret
+
+@method_decorator(login_required, name='dispatch')
+class ApplicationDetail(ApplicationListMixin, DetailView):
+    model = Application
+
+
+@method_decorator(login_required, name='dispatch')
+class SubscriptionList(ApplicationListMixin, ListView):
+    model = Subscription
+
+    def get_queryset(self):
+        return Subscription.objects.filter(subscriber=self.request.user)
