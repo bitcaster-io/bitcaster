1932fbc32762f1febb58637bf32722e8297414e5
sax <s.apostolico@gmail.com>
2018-07-07 09:11:42 +0200

    fixes docker demo image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 
-if [ ! -f /var/bitcaster/.bootstrapped ]; then
-    touch /var/bitcaster/.bootstrapped
-    echo "done"
-fi
+
 if [ "$@" == "bitcaster" ];then
     pg-control start
     redis-server &
-    bitcaster configure --no-input
-    bitcaster upgrade --no-input
-    bitcaster start web -l debug --bind 0.0.0.0:8000 &
+    if [ ! -f /var/bitcaster/.bootstrapped ]; then
+        touch /var/bitcaster/.bootstrapped
+        bitcaster configure --no-input
+        bitcaster upgrade --no-input
+        echo "done"
+    fi
     bitcaster start workers -l debug &
+    exec bitcaster start web -l debug --bind 0.0.0.0:8000
 else
     exec "$@"
 fi
