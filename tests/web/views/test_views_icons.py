fd308ac02fc442a784c7b20d6779642c9c8c9fd4
sax <s.apostolico@gmail.com>
2019-03-06 01:42:38 +0100

    add tests

--- tests/web/views/test_views_icons.py
+++ tests/web/views/test_views_icons.py
@@ -3,7 +3,7 @@ from strategy_field.utils import fqn
 
 from bitcaster.dispatchers import Email, Gmail
 from bitcaster.dispatchers.handlers.twitter import TwitterDirectMessage
-from bitcaster.web.views.icons import plugin_icon
+from bitcaster.web.views.icons import channel_icon, plugin_icon
 
 
 @pytest.mark.django_db
@@ -11,3 +11,9 @@ from bitcaster.web.views.icons import plugin_icon
 def test_plugin_icon(rf, target):
     request = rf.get('/')
     assert plugin_icon(request, target)
+
+
+@pytest.mark.django_db
+def test_channel_icon(rf, channel1):
+    request = rf.get('/')
+    assert channel_icon(request, channel1.pk)
