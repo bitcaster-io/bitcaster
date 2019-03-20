99f7e4f1cc03844e9969ee30d61324d345e6fb84
GigiusB <g.bronzini@gmail.com>
2019-03-20 00:58:48 +0000

    Finally merged

--- src/bitcaster/config/urls.py
+++ src/bitcaster/config/urls.py
@@ -4,6 +4,7 @@ from urllib.parse import parse_qs
 from django.conf import settings
 from django.conf.urls import include
 from django.contrib.admin import site
+from django.contrib.auth.decorators import login_required
 from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
 from django.views.static import serve
@@ -28,9 +29,17 @@ def channel_callback(request, pk):
     return channel.handler.callback(request)
 
 
+@login_required
+def channel_registration(request, pk):
+    from bitcaster.models import Channel
+    channel = Channel.objects.get(pk=pk)
+    return channel.handler.registration(request)
+
+
 urlpatterns = [path('api/', include(bitcaster.api.urls), name='api'),
                path('oauth2callback/', oauth2callback),
                path('channel-callback/<int:pk>/', channel_callback, name='channel-callback'),
+               path('channel-registration/<int:pk>/', channel_registration, name='channel-registration'),
                path('admin/info/html/', admin_sysinfo, name='admin_info'),
                path('info/json/', http_basic_login(sysinfo), name='sys-info'),
 
