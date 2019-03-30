b180fcd24f7c598cf797ad66a7695ea580ff6a92
sax-os4d <sax@os4d.org>
2019-03-30 12:24:36 +0100

    Bump version: 0.7.0a8 → 0.7.0a9

--- src/bitcaster/__init__.py
+++ src/bitcaster/__init__.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 DIR = pathlib.Path(__file__).parent
 
 NAME = 'bitcaster'
-VERSION = __version__ = '0.7.0a8'
+VERSION = __version__ = '0.7.0a9'
 __author__ = 'Stefano Apostolico'
 
 
