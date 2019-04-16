ec5aa27a0cb5d573a00cc6d4e87da7a203be6ce1
sax-os4d <sax@os4d.org>
2019-04-16 16:23:20 +0200

    Bump version: 0.14.0a11 → 0.14.0a12

--- src/bitcaster/__init__.py
+++ src/bitcaster/__init__.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 DIR = pathlib.Path(__file__).parent
 
 NAME = 'bitcaster'
-VERSION = __version__ = '0.14.0a11'
+VERSION = __version__ = '0.14.0a12'
 __author__ = 'Stefano Apostolico'
 
 
