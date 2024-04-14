af1bec03eb6291e808c20f0a45cb544615a98172
Mark Hanarero <hanarero@gmail.com>
2024-04-14 23:34:31 +0200

    updates

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -34,13 +34,22 @@ def test_upgrade_check(mocked_responses):
 
 
 @pytest.mark.parametrize("verbosity", [0, 1], ids=["0", "1"])
-def test_env(mocked_responses, verbosity):
+@pytest.mark.parametrize("develop", [0, 1], ids=["0", "1"])
+@pytest.mark.parametrize("diff", [0, 1], ids=["0", "1"])
+@pytest.mark.parametrize("config", [0, 1], ids=["0", "1"])
+@pytest.mark.parametrize("check", [0, 1], ids=["0", "1"])
+def test_env(mocked_responses, verbosity, develop, diff, config, check):
     out = StringIO()
     environ = {"ADMIN_URL_PREFIX": "test"}
     with mock.patch.dict(os.environ, environ, clear=True):
         call_command(
             "env",
+            ignore_errors=True if check == 1 else False,
             stdout=out,
             verbosity=verbosity,
+            develop=develop,
+            diff=diff,
+            config=config,
+            check=check,
         )
         assert "error" not in str(out.getvalue())
