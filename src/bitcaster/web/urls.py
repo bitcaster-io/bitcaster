03522c93a7820907e2bdffd4b8f9ef721b6dadc4
alex <alex@os4d.org>
2024-05-09 21:47:39 +0200

    add media file serving

--- src/bitcaster/web/urls.py
+++ src/bitcaster/web/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+import re
+
+from django.conf import settings
+from django.urls import path, re_path
 
 from . import views
+from .views import MediaView
 
 urlpatterns = [
     path("", views.index, name="home"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("admin/logout/", views.LogoutView.as_view(), name="logout"),
+    re_path(r"^%s(?P<path>.*)$" % re.escape(settings.MEDIA_URL.lstrip("/")), MediaView.as_view()),
 ]
