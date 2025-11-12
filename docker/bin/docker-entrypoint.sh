a98ef7bf8f7a5e366ca445332a14fb786a4d5b36
Giovanni B. <g.bronzini@gmail.com>
2025-11-12 10:17:45 +0000

    Fixed wrong user in entrypoint (#62)

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -10,17 +10,17 @@ echo $STATIC_ROOT
 echo $MEDIA_ROOT
 
 if [ -d "${STATIC_ROOT}" ];then
-  chown -R bitcaster:bitcaster-io ${STATIC_ROOT}
+  chown -R os4d:bitcaster ${STATIC_ROOT}
 fi
 if [ -d "${MEDIA_ROOT}" ];then
-  chown -R bitcaster:bitcaster-io ${MEDIA_ROOT}
+  chown -R os4d:bitcaster ${MEDIA_ROOT}
 fi
 
 case "$1" in
     upgrade)
       django-admin check --deploy
 	    set -- tini -- "$@"
-      set -- gosu bitcaster:bitcaster-io django-admin upgrade
+      set -- gosu os4d:bitcaster django-admin upgrade
       ;;
     config)
       echo "CMD $@"
@@ -29,22 +29,22 @@ case "$1" in
       ;;
     flower)
 	    set -- tini -- "$@"
-      set -- gosu bitcaster:bitcaster-io celery -A bitcaster.config.celery flower
+      set -- gosu os4d:bitcaster celery -A bitcaster.config.celery flower
       ;;
     worker)
 	    set -- tini -- "$@"
-      set -- gosu bitcaster:bitcaster-io celery -A bitcaster.config.celery worker -E --loglevel=ERROR --concurrency=4
+      set -- gosu os4d:bitcaster celery -A bitcaster.config.celery worker -E --loglevel=ERROR --concurrency=4
       ;;
     beat)
 	    set -- tini -- "$@"
-      set -- gosu bitcaster:bitcaster-io celery -A bitcaster.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler
+      set -- gosu os4d:bitcaster celery -A bitcaster.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler
       ;;
     run)
       django-admin check --deploy
       django-admin upgrade
-      chown -R bitcaster:bitcaster-io ${STATIC_ROOT}
+      chown -R os4d:bitcaster ${STATIC_ROOT}
 	    set -- tini -- "$@"
-  		set -- gosu bitcaster:bitcaster-io uwsgi --ini /conf/uwsgi.ini
+  		set -- gosu os4d:bitcaster uwsgi --ini /conf/uwsgi.ini
 	    ;;
 esac
 
