32ec82e13da72b13f0fb821b261d1febed432fa2
s.apostolico <s.apostolico@gmail.com>
2026-01-05 11:49:02 +0100

    fixes typo

--- src/bitcaster/config/__init__.py
+++ src/bitcaster/config/__init__.py
@@ -55,7 +55,7 @@ CONFIG: "Mapping[str, ConfigItem]" = {
         "https://django-environ.readthedocs.io/en/latest/types.html#environ-env-db-url",
         False,
     ),
-    "DRAMATIQ_BROKER": (str, "redis://dramatiq-broker:6379/0", "", True),
+    "DRAMATIQ_BROKER": (str, "redis://dramatiq-broker:6379/0", "", "redis://dramatiq-broker:6379/0"),
     "DEBUG": (bool, False, setting("debug"), True),
     "EMAIL_BACKEND": (str, "django.core.mail.backends.smtp.EmailBackend", setting("email-backend"), True),
     "EMAIL_HOST": (str, "localhost", setting("email-host"), True),
