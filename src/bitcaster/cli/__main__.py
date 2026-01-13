13cdb620510a10745f66875ac4aa98eb1a88a34d
max <max@os4d.org>
2026-01-13 21:44:09 +0100

    add --version option to bc

--- src/bitcaster/cli/__main__.py
+++ src/bitcaster/cli/__main__.py
@@ -1,4 +1,3 @@
-import logging
 import logging.config
 import os
 from typing import TYPE_CHECKING
@@ -6,6 +5,8 @@ from typing import TYPE_CHECKING
 import click
 import django
 from django.conf import settings
+
+from ..version import __version__
 
 if TYPE_CHECKING:
     from click.core import Context
@@ -19,6 +20,7 @@ except Exception as e:
 
 
 @click.group()
+@click.version_option(__version__)
 @click.option("--debug", default=False, is_flag=True, envvar="BITCASTER_DEBUG")
 @click.pass_context
 def cli(ctx: "Context", debug: bool) -> None:
