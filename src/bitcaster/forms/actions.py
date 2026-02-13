0cd52ae6a1d4f5dac2b11ba930e5690b53930b8a
alex <alex@os4d.org>
2026-02-13 08:12:58 +0100

    code clean

--- src/bitcaster/forms/actions.py
+++ src/bitcaster/forms/actions.py
@@ -1,12 +1,6 @@
 from django import forms
+from django.contrib.admin.helpers import ActionForm
 
 
-class GenericActionForm(forms.Form):
+class GenericActionForm(ActionForm):
     _selected_action = forms.CharField(label="", widget=forms.MultipleHiddenInput)
-    select_across = forms.BooleanField(
-        label="",
-        required=False,
-        initial=0,
-        widget=forms.HiddenInput({"class": "select-across"}),
-    )
-    action = forms.CharField(label="", required=True, initial="", widget=forms.HiddenInput())
