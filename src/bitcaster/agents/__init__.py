52346ab88108d01b7ea0df8826b48e744bb1fd12
hanarero <hanarero@gmail.com>
2026-05-02 16:05:40 +0200

    feat(agents): add AgentImap for monitoring IMAP mailboxes (#174)

--- src/bitcaster/agents/__init__.py
+++ src/bitcaster/agents/__init__.py
@@ -1,7 +1,9 @@
 from .fs import AgentFileSystem  # noqa
 from .ftp import AgentFTP  # noqa
+from .imap import AgentImap  # noqa
 
 __all__ = [
     "AgentFTP",
     "AgentFileSystem",
+    "AgentImap",
 ]
